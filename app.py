4357372187cfc509dede8d4c53dbd8b8ae6b4144
Kenneth Woanyah <kenneth.woanyah@gmail.com>
2023-12-02 12:54:30 -0500

    s

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ if not open_AI_key:
 #model = MusicGen.get_pretrained('facebook/musicgen-small')
 #model = musicgen.MusicGen.get_pretrained('small', device='cpu')
 
-model = musicgen.MusicGen.get_pretrained('medium', device='cpu')
+model = musicgen.MusicGen.get_pretrained('small', device='cpu')
 model.set_generation_params(duration=8)
 
 # model.set_generation_params(
